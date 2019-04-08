0038895a06fa1cd34f8d796fad7925e3cff7e1cc
RaphaelAzev <raphaelazev@hotmail.com>
2019-04-08 15:43:40 -0300

    Nada, tristeza so, e um pouco de tentativas de fazer botao oled funfar

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -11,6 +11,18 @@
 #include "calibri_36.h"
 #include "arial_72.h"
 
+#define BUT_PIO_ID			  ID_PIOD
+#define BUT_PIO				  PIOD
+#define BUT_IDX				  27
+#define BUT_IDX_MASK		  (1 << BUT_IDX)
+
+// LED
+#define LED_PIO      PIOC
+#define LED_PIO_ID   ID_PIOC
+#define LED_IDX      8
+#define LED_IDX_MASK (1 << LED_IDX)
+
+
 
 struct ili9488_opt_t g_ili9488_display_opt;
 
@@ -27,6 +39,16 @@ void configure_lcd(void){
 	
 }
 
+void but_callback(void)
+{
+	for (int i=0;i<5;i++)
+	{
+		pio_clear(LED_PIO, LED_IDX_MASK);
+		delay_ms(200);
+		pio_set(LED_PIO, LED_IDX_MASK);
+		delay_ms(200);
+	}
+}
 
 void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
 	char *p = text;
@@ -42,6 +64,37 @@ void font_draw_text(tFont *font, const char *text, int x, int y, int spacing) {
 	}	
 }
 
+void io_init(void)
+{
+	// Configura led
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_IDX_MASK, PIO_DEFAULT);
+	
+	// Inicializa clock do periférico PIO responsavel pelo botao
+	pmc_enable_periph_clk(BUT_PIO_ID);
+
+	// Configura PIO para lidar com o pino do botão como entrada
+	// com pull-up
+	pio_configure(BUT_PIO, PIO_INPUT, BUT_IDX_MASK, PIO_PULLUP);
+
+	// Configura interrupção no pino referente ao botao e associa
+	// função de callback caso uma interrupção for gerada
+	// a função de callback é a: but_callback()
+	pio_handler_set(BUT_PIO,
+	BUT_PIO_ID,
+	BUT_IDX_MASK,
+	PIO_IT_RISE_EDGE,
+	but_callback);
+
+	// Ativa interrupção
+	pio_enable_interrupt(BUT_PIO, BUT_IDX_MASK);
+
+	// Configura NVIC para receber interrupcoes do PIO do botao
+	// com prioridade 4 (quanto mais próximo de 0 maior)
+	NVIC_EnableIRQ(BUT_PIO_ID);
+	NVIC_SetPriority(BUT_PIO_ID, 4); // Prioridade 4
+}
+
 
 int main(void) {
 	board_init();
@@ -52,6 +105,8 @@ int main(void) {
 	font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
 	font_draw_text(&arial_72, "102456", 50, 200, 2);
 	while(1) {
-		
+		if (!pio_get(BUT_PIO, PIO_INPUT, BUT_IDX_MASK)) {
+			
+		}
 	}
 }
